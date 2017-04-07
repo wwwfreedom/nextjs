0300508759bb0a634fb698cb6a3298232f936fa3
Kevin <tig3r86@gmail.com>
2017-04-07 21:42:11 +1000

    Add postcss with css-next for css styling

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,25 @@ import React from 'react'
 
 export default function index() {
   return (
-    <div>
-      hello world
+    <div className='hello'>
+      <p>Hello World</p>
+      <style jsx>{`
+      :global(:root) {
+        --bgColor: blue;
+        --color: white;
+      }
+      .hello {
+        font: 15px Helvetica, Arial, sans-serif;
+        background: var(--bgColor);
+        color: var(--color);
+        padding: 100px;
+        text-align: center;
+        transition: 100ms ease-in background;
+      }
+      .hello:hover {
+        color: color(var(--color) blackness(+80%));
+      }
+    `}</style>
     </div>
   )
 }
